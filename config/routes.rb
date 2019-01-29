cbd03fc646677e23ef7f722c3c9b966185b7f036
Alex Aslam <alexaslam02@hotmail.com>
2019-01-30 00:25:35 +0500

    reservation controller create action added and strong parameters for reservation api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  resources :guests
+  get 'reservation/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
